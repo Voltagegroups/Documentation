2724b5b736ecc055d7b635eda0793b35e3b0a96e
LilianVuillamier <virvoltasurmc@outlook.fr>
2022-09-22 22:58:42 +0200

    update theme

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -1,3 +1,5 @@
+import sphinx_theme_pd
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -19,10 +21,8 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = 'sphinx_theme_pd'
+html_theme_path = [sphinx_theme_pd.get_html_theme_path()]

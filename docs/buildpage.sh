cc88557b01b79f53d89ecf90c4de5c48ce4d64b6
LilianVuillamier <virvoltasurmc@outlook.fr>
2022-09-23 09:50:32 +0200

    test new build

---
+++ docs/buildpage.sh
@@ -0,0 +1,21 @@
+apt-get update
+apt-get install --no-install-recommends -y \
+      python3-sphinx \
+      graphviz \
+      imagemagick \
+      make
+apt-get autoremove
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+
+pwd ls -lah
+export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
+
+##############
+# BUILD DOCS #
+##############
+
+# Python Sphinx, configured with source/conf.py
+# See https://www.sphinx-doc.org/
+make clean
+make html
